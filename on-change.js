83d7e779baf4ff4a2b726ad10a447e966c80d93d
Eric Zhang <ericzhang135@users.noreply.github.com>
2019-03-18 21:11:36 +0800

    Trying to fix some no-update bug
    That is not a good trick to play with, I guess...
    
    And with mixed indent fixed.

--- on-change.js
+++ on-change.js
@@ -1,21 +1,25 @@
 //Modified from https://davidwalsh.name/watch-object-changes
 
 export function onChange(obj, callback) {
-	const handler = {
-		get(target, property, receiver) {
-			try {
-				return new Proxy(target[property], handler);
-			} catch (err) {
-				return Reflect.get(target, property, receiver);
-			}
-		},
-		defineProperty(target, property, descriptor) {
-			return Reflect.defineProperty(target, property, descriptor) && (callback() || 1);
-		},
-		deleteProperty(target, property) {
-			return Reflect.deleteProperty(target, property) && (callback() || 1);
-		}
-	};
+    const handler = {
+        get(target, property, receiver) {
+            try {
+                return new Proxy(target[property], handler);
+            } catch (err) {
+                return Reflect.get(target, property, receiver);
+            }
+        },
+        defineProperty(target, property, descriptor) {
+            var ret = Reflect.defineProperty(target, property, descriptor);
+            callback();
+            return ret;
+        },
+        deleteProperty(target, property) {
+            var ret = Reflect.deleteProperty(target, property);
+            callback();
+            return ret;
+        }
+    };
     callback();
-	return new Proxy(obj, handler);
+    return new Proxy(obj, handler);
 };
